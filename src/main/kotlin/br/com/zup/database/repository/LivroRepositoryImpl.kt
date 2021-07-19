e293f2943718f2e2d531ffff02ac55abe25f52a0
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-18 22:23:00 -0300

    Criar LivroRepository

--- src/main/kotlin/br/com/zup/database/repository/LivroRepositoryImpl.kt
+++ src/main/kotlin/br/com/zup/database/repository/LivroRepositoryImpl.kt
@@ -1,4 +1,51 @@
 package br.com.zup.database.repository
 
-class LivroRepositoryImpl {
+import br.com.zup.core.ports.LivroRepository
+import br.com.zup.database.entity.LivroEntity
+import java.lang.RuntimeException
+import java.util.*
+
+
+class LivroRepositoryImpl(private val cqlSession: CqlSession) : LivroRepository {
+
+    override fun getAll(): List<LivroEntity> {
+        val queryResult = cqlSession.execute(
+            SimpleStatement
+                .newInstance(
+                    "SELECT * FROM livraria.Livros",
+                )
+        )
+        return queryResult.map { livro ->
+            LivroEntity(
+                livro.getUuid("id")!!,
+                livro.getString("nome")!!,
+                livro.getString("nomeAutor")!!,
+                livro.getString("anoDePublicacao")!!,
+            )
+        }.toList()
+    }
+
+    override fun getById(id: UUID): LivroEntity? {
+        try {
+            val queryResult = cqlSession.execute(
+                SimpleStatement
+                    .newInstance(
+                        "SELECT * FROM livraria.Livros WHERE is =?",
+                        id
+                    )
+            )
+            return queryResult.map { livro ->
+                LivroEntity(
+                    livro.getUuid("id")!!,
+                    livro.getString("nome")!!,
+                    livro.getString("nomeAutor")!!,
+                    livro.getString("anoDePublicacao")!!,
+                )
+            }.firstOrNull()
+
+        }catch (e: RuntimeException){
+            throw RuntimeException()
+        }
+
+    }
 }
