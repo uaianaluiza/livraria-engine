6873909c84f76ae6a3b3cdb08d638be94b4fbea6
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-18 21:01:25 -0300

    Criar LivroEntity

--- src/main/kotlin/br/com/zup/database/entity/LivroEntity.kt
+++ src/main/kotlin/br/com/zup/database/entity/LivroEntity.kt
@@ -1,4 +1,10 @@
 package br.com.zup.database.entity
 
-class LivroEntity {
-}
+import java.util.*
+
+data class LivroEntity (
+    val id: UUID? = null,
+    val nome: String = "",
+    val nomeAutor: String = "",
+    val anoDePublicacao: String = "",
+        )
