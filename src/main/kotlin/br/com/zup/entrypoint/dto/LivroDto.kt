0802f6ea2a474c263f3a2a2561c8157da8d561fc
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-07-18 21:31:32 -0300

    Criar LivroDto

--- src/main/kotlin/br/com/zup/entrypoint/dto/LivroDto.kt
+++ src/main/kotlin/br/com/zup/entrypoint/dto/LivroDto.kt
@@ -1,4 +1,10 @@
 package br.com.zup.entrypoint.dto
 
-class LivroDto {
-}
+import java.util.*
+
+data class LivroDto (
+    val id: UUID? = null,
+    val nome: String = "",
+    val nomeAutor: String = "",
+    val anoDePublicacao: String = "",
+    )
